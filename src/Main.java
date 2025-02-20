cfd0b98f9050dc0124e9d574dfc8c55281dc88b2
David <dayeni@csub.edu>
2025-02-20 11:36:46 -0800

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,6 @@ import javax.swing.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
         startView app = new startView();
         JPanel start = app.getStartPanel();
